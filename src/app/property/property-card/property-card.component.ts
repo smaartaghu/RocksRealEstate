5367af58caeda46498a502556d19f58ccc925ae5
Raghavendra V <raghu.vs2007@hotmail.com>
2020-12-01 17:36:09 +0530

    Home page created

---
+++ src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,18 @@
+
+import { style } from '@angular/animations';
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-property-card',
+ // template: `<h1>Hi This is a card</h1>`,
+ templateUrl: 'property-card.component.html',
+// styles:  [``]
+styleUrls:['property-card.component.css']
+
+}
+)
+export class ProperyCardComponent{
+
+}
